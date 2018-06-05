b474ffa90f5bdb4c400d95f61608828427b54216
taeiim <ti535666@gmail.com>
2018-06-05 16:21:41 +0900

    Add StreamWriter_TodoList

---
+++ C#/StreamWriter_TodoList/StreamWriterEx/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace StreamWriterEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //StreamWriter writer = new System.IO.StreamWriter(@"\Users\parktaeim\temp\abc.txt", true);
+            //writer.Write("시험중입니다.");
+            //writer.WriteLine("두번째 시험중입니다 .");
+            //writer.WriteLine("세번째 시험중입니다 .");
+            //writer.Close();
+
+            //StreamReader streamReader = new StreamReader(@"\Users\parktaeim\temp\abc.txt", true);
+            //while(!streamReader.EndOfStream){
+            //    string str = streamReader.ReadLine();
+            //    Console.WriteLine("내용:" + str);
+            //}
+            //streamReader.Close();
+
+
+            new ToDoList();
+
+            //using (StreamWriter writer = new System.IO.StreamWriter(@"/Users/parktaeim/temp/test.txt",true)){
+            //    writer.Write("시험중입니다.");
+            //    writer.WriteLine("두번째 시험중입니다 .");
+            //}
+        }
+    }
+}
---
+++ C#/StreamWriter_TodoList/StreamWriterEx/ToDoList.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Collections.Generic;
+
+namespace StreamWriterEx
+{
+    public class ToDoList
+    {
+        public ToDoList()
+        {
+            startToDoList();
+        }
+
+        public void startToDoList(){
+            while(true){
+                Console.WriteLine();
+                Console.WriteLine("1.열기\t2.할일 저장\t3.종료");
+                int num = int.Parse(Console.ReadLine());
+                if (num == 3) break;
+                switch(num){
+                    case 1:
+                        taskFileOpen();
+                        break;
+                    case 2:
+                        saveTask();
+                        break;
+                }
+            }
+        }
+
+        public void taskFileOpen(){
+            List<string> fileNameList = new List<string>();
+            int i = 1;
+
+            string folderPath = @"/Users/parktaeim/Desktop/temp";
+            if(System.IO.Directory.Exists(folderPath)){
+                System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(folderPath);
+                foreach (System.IO.FileInfo File in di.GetFiles())
+                {
+                    if (File.Extension.ToLower().CompareTo(".txt") == 0)
+                    {
+                        String FileNameOnly = File.Name.Substring(0, File.Name.Length - 4);
+                        String FullFileName = File.FullName;
+
+                        Console.WriteLine("["+i+"] "+FullFileName + "  " + FileNameOnly);
+
+                        fileNameList.Add(FileNameOnly);
+                        i++;
+                    }
+                }
+            }
+
+            Console.Write("오픈하실 파일을 선택해주세요 : ");
+            int fileNum = int.Parse(Console.ReadLine());
+            string filename = fileNameList[fileNum - 1];
+            StreamReader streamReader = new StreamReader(@"/Users/parktaeim/Desktop/temp/" +filename+ ".txt", true);
+            while (!streamReader.EndOfStream)
+            {
+                string str = streamReader.ReadLine();
+                Console.WriteLine(str);
+            }
+            streamReader.Close();
+
+        }
+
+        public void saveTask(){
+            string taskStr;
+            string finishDate;
+            string import;
+            string detailStr;
+
+            Console.Write("할 일 : ");
+            taskStr = Console.ReadLine();
+            Console.Write("기한 : ");
+            finishDate = Console.ReadLine();
+            Console.Write("중요도 : ");
+            import = Console.ReadLine();
+            Console.Write("상세설명 : ");
+            detailStr = Console.ReadLine();
+            Console.WriteLine("=====저장완료=====");
+
+            StreamWriter writer = new System.IO.StreamWriter(@"/Users/parktaeim/Desktop/temp/" + taskStr + ".txt", true);
+            writer.WriteLine("할 일 : " + taskStr);
+            writer.WriteLine("기한 : " + finishDate);
+            writer.WriteLine("중요도 : " + import);
+            writer.WriteLine("상세설명 : " + detailStr);
+            writer.Close();
+        }
+
+    }
+}
